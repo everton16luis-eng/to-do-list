20d017ee11330c6ab5bc0ca7d548715b9d2ce86f
Everton Vieira <everton16luis@gmail.com>
2026-04-02 16:20:28 -0300

    Criado funções para armazenar e carregar o arquivo json no localStorage, ajustado alguns elementos visuais do sistema

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 function addTask() {
     const container = document.getElementById("container");
-    //captura o texto dentro da caixa de task
     const task = document.querySelector('#tarefas')
-    const texto = task.value
+    const tarefa = task.value
     const taskDate = document.querySelector('#data').value
     const date = new Date(taskDate)
-    data = date.toLocaleString("pt-BR")
-    const Solicitado = "Fernanda Montenegro"
+    const hoje = new Date()
+    const usuario = "Fernanda Montenegro"
+    prazo = date.toLocaleString("pt-BR")
+    //verificações da regra de negocio
     if (task.value == "" || taskDate == "") {
         alert("Por favor preencha todos os campos!")
+        return
+    }
+    if (date < hoje) {
+        alert("Data limite deve ser maior que a data atual!")
         return
     }
 
@@ -18,11 +23,11 @@ function addTask() {
         // adiciona conteúdo dentro dela
         ` <div class="Task">
         <div class="text">
-            <p>` + texto + ` </p>
+            <p>` + tarefa + ` </p>
             <div class="traco"></div>
             <div class="responsavel">
-                <p>Solicitado por: ` + Solicitado + `</p>
-                <P>Prazo: ` + data + `</P>
+                <p>Solicitado por: ` + usuario + `</p>
+                <P>Prazo: ` + prazo + `</P>
             </div>
         </div>
         <div class="butons">
@@ -32,8 +37,7 @@ function addTask() {
                     title="Remover Task" onclick="remover(this)">
                 </div>
         </div>`
-
-    //Limpa o texto dentro do input de tasks
+    armazenar(tarefa, usuario, prazo)
     clearField('#tarefas')
     clearField('#data')
 
@@ -58,7 +62,6 @@ function concluir(botao) {
   listaConcluidas.appendChild(tarefa);
   
 }
-
 //Remove a tarefa programada
 function remover(botao) {
   botao.closest(".Task").remove();
@@ -68,3 +71,58 @@ function remover(botao) {
 
   listaConcluidas.appendChild(tarefa);
 }
+
+function armazenar(tarefa, usuario, prazo){
+    const jsTarefa = {
+        tarefa: tarefa,
+        usuario: usuario,
+        prazo: prazo,
+    };
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+    tarefas.push(jsTarefa);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+}
+function loadTarefas(){
+    const atrasadas = document.getElementById("atrasadas");
+    const agora = new Date()
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    const referencia = document.getElementById("ref");
+    tarefas.forEach(tarefa => {
+         const card = document.createElement("div");
+         console.log(agora.toLocaleString(), tarefa.prazo)
+    card.innerHTML =
+        // adiciona conteúdo dentro dela
+        ` <div class="Task">
+        <div class="text">
+            <p>` + tarefa.tarefa + ` </p>
+            <div class="traco"></div>
+            <div class="responsavel">
+                <p>Solicitado por: ` + tarefa.usuario + `</p>
+                <P>Prazo: ` + tarefa.prazo + `</P>
+            </div>
+        </div>
+        <div class="butons">
+            <img class="confirm" width="20" height="20" src="css/img/icons/check.png" alt="Task Concluida"
+                title="Task Concluida" onclick="concluir(this)">
+                <img class="confirm" width="20" height="20" src="css/img/icons/remove.png" alt="Remover"
+                    title="Remover Task" onclick="remover(this)">
+                </div>
+        </div>`
+    
+    // insere antes do elemento de referência
+    if (tarefa.prazo > agora.toLocaleString() ){
+        container.insertBefore(card, referencia);
+    }else{
+        atrasadas.insertBefore(card, referencia)
+
+    }
+    
+    });
+}
+
+document.addEventListener("DOMContentLoaded",() =>{
+    loadTarefas()
+}
+)
